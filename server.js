03b0dca0005fdd591b8f98d9a57ca0432e32e38f
Nishant <sapkota.nishant@wmich.edu>
2020-09-15 17:13:47 -0400

    Folders added

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const router = require('./routes/post');
 const app = express()
 dotenv.config()
 app.listen(process.env.PORT, (req, res)=>{
   console.log("listening on port", process.env.PORT);
 })
-app.get('/', (req, res)=>{
-  res.send('Hello world!!')
-});
+app.use('/api', router);

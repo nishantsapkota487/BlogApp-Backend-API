07a07579c098f1892cb7642a1d930936d08abda1
Nishant <sapkota.nishant@wmich.edu>
2020-09-16 23:39:57 -0400

    User model created

--- models/user.js
+++ models/user.js
@@ -1,1 +1,25 @@
 // USER MODEL WILL BE HERE
+const express = require('express');
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema;
+const userModel = schema({
+  username:{
+    type:String,
+    required:true,
+    min:6,
+    max:56,
+  },
+  email:{
+    type:String,
+    required:true,
+  },
+  password:{
+    type:String,
+    required:true,
+    min:5,
+    max:56
+  }
+});
+
+module.exports = mongoose.model('User', userModel);
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const router = require('./routes/post');
-const app = express()
-dotenv.config()
+const userSchema = require('./models/User');
+const app = express();
+dotenv.config();
 app.listen(process.env.PORT, (req, res)=>{
   console.log("listening on port", process.env.PORT);
 })
+mongoose.connect(process.env.DB_CONNECT,
+   { useUnifiedTopology: true,
+     useNewUrlParser: true,
+    },
+   ()=>{
+  console.log('DB Connected');
+});
 app.use('/api', router);
